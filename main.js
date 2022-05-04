593eeb047bcf461e58ef3e14e52ddda55cce752e
Emilio Pino <pinoen@yahoo.com.ar>
2022-05-04 17:15:27 -0300

    Add playSound and animatePress functions

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
@@ -10,11 +11,35 @@ let randomChosenColour = buttonColours[nextSequence()];
 
 gamePattern.push(randomChosenColour);
 
-let activeButton = document.querySelector(`#${randomChosenColour}`);
-activeButton.classList.add("pressed");
-setTimeout(() => {
-  activeButton.classList.remove("pressed");
-}, 300);
+animatePress(randomChosenColour);
+// let activeButton = document.querySelector(`#${randomChosenColour}`);
+// activeButton.classList.add("pressed");
+// setTimeout(() => {
+//   activeButton.classList.remove("pressed");
+// }, 300);
 
-let colorSound = new Audio(`sounds/${randomChosenColour}.mp3`);
-colorSound.play();
+playSound(randomChosenColour);
+
+let clickedButton = document.querySelectorAll(".btn");
+
+clickedButton.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let userChosenColour = btn.id;
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+  });
+});
+
+function playSound(name) {
+  let colorSound = new Audio(`sounds/${name}.mp3`);
+  colorSound.play();
+}
+
+function animatePress(currentColor) {
+  let activeButton = document.querySelector(`#${currentColor}`);
+  activeButton.classList.add("pressed");
+  setTimeout(() => {
+    activeButton.classList.remove("pressed");
+  }, 150);
+}
