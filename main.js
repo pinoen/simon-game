f0420dff53d24e02f61e95f3e44275a1ad8e3d07
Emilio Pino <pinoen@yahoo.com.ar>
2022-05-04 15:51:12 -0300

    Add function to choose random number tied to color

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
 
+function nextSequence() {
+  let randomNumber = Math.floor(Math.random() * 4);
+  return randomNumber;
+}
 
+let randomChosenColour = buttonColours[nextSequence()];
+
+gamePattern.push(randomChosenColour);
